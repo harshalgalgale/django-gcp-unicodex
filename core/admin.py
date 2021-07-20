b3e1a8a0fefbe2628eeca99d0cc88fe676ffbe47
harshalgalgale <h.galgale@gmail.com>
2021-07-20 19:33:21 +0100

    Removed import-export admin

--- core/admin.py
+++ core/admin.py
@@ -3,24 +3,26 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from core.models import MainSector, SubSector, Skills
+admin.site.register(MainSector)
+admin.site.register(SubSector)
+admin.site.register(Skills)
 
-
-@admin.register(Skills)
-class SkillsAdmin(ImportExportModelAdmin):
-    model = Skills
-
-    save_as = True
-
-
-@admin.register(MainSector)
-class MainSectorAdmin(ImportExportModelAdmin):
-    model = MainSector
-
-    save_as = True
-
-
-@admin.register(SubSector)
-class SubSectorAdmin(ImportExportModelAdmin):
-    model = SubSector
-
-    save_as = True
+# @admin.register(Skills)
+# class SkillsAdmin(ImportExportModelAdmin):
+#     model = Skills
+#
+#     save_as = True
+#
+#
+# @admin.register(MainSector)
+# class MainSectorAdmin(ImportExportModelAdmin):
+#     model = MainSector
+#
+#     save_as = True
+#
+#
+# @admin.register(SubSector)
+# class SubSectorAdmin(ImportExportModelAdmin):
+#     model = SubSector
+#
+#     save_as = True
